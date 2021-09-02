828cea5740a624a3ff63c241b3c03180ff5dc591
Zaah91 <zaah91@gmail.com>
2021-09-02 12:25:32 +0200

    Commit 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hell yeah, its working!");
-        System.out.println("hjhasefj");
 
 	// write your code here
+    // Normal single line comment
+
+    /*
+       Multiple lines comments
+    */
+
     }
 }
