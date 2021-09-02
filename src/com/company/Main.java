cc99fec9823060035988f835f094a22e4be6e0d1
allan <89967486+alla2297@users.noreply.github.com>
2021-09-02 14:43:19 +0200

    path 2
    tilføjet: is it

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hell yeah, its working!");
+	System.out.println("is it?");
 
 	// write your code here
     // Normal single line comment
