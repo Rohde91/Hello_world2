c167714324898ca5f03f9fa8e3a05db192801017
Zaah91 <zaah91@gmail.com>
2021-09-05 13:15:54 +0200

    Assignment 2 end

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,7 +18,32 @@ public class Main {
         System.out.print(hour);
         System.out.print(":");
         System.out.print(minute);
-        System.out.print(".");
+        System.out.println(".");
 
+        String day = "sunday ";
+        String date = "5 th ";
+        String month = "sep ";
+        String year = "2021";
+        System.out.println("American format:");
+        System.out.print(date);
+        System.out.print(day);
+        System.out.print(month);
+        System.out.println(year);
+
+        System.out.println("European format:");
+        System.out.print(day);
+        System.out.print(date);
+        System.out.print(month);
+        System.out.println(year);
+        System.out.println(day+date+month+year);
+        System.out.println("Made whit a different print style!");
+
+        int second = 23;
+
+        System.out.print("Seconds since midnight: ");
+        System.out.println(hour * 60 * 60 + minute * 60 + second);
+
+        System.out.print("Seconds left of the day: ");
+        System.out.println(24 * 60 * 60 - hour * 60 * 60 - minute * 60 - second);
     }
 }
